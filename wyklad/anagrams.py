55e24990ef3db1ac87bb7cc694c56efb733fe5a1
Pooler <180275@edu.p.lodz.pl>
2016-10-24 10:09:05 +0200

    Create anagrams.py

---
+++ wyklad/anagrams.py
@@ -0,0 +1,15 @@
+from functools import reduce
+
+
+def is_anagram(a, b):
+    return sorted(a) == sorted(b)
+
+
+print(is_anagram("asd", "dsa"), is_anagram("asd", "dsa."))
+
+
+def is_anagram_ext(a, *b):
+    return sorted(a) == sorted(b[0]) if len(b) else reduce((lambda x, y: sorted(x) if (is_anagram(x, y)) else ""), b)
+
+
+print(is_anagram_ext("asd", "dsa", "sda"), is_anagram_ext("asd", "sda"), is_anagram_ext("asd", "sda", "dsa."))
