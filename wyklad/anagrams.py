96e0330e1c06d9ec992a3aff975cec8c2e7c57b6
Pooler <180275@edu.p.lodz.pl>
2016-10-24 10:32:31 +0200

    Update anagrams.py

--- wyklad/anagrams.py
+++ wyklad/anagrams.py
@@ -9,7 +9,7 @@ print(is_anagram("asd", "dsa"), is_anagram("asd", "dsa."))
 
 
 def is_anagram_ext(a, *b):
-    return sorted(a) == sorted(b[0]) if len(b) else reduce((lambda x, y: sorted(x) if (is_anagram(x, y)) else ""), b)
+    return sorted(a) == sorted(b[0]) if (len(b) == 1) else reduce((lambda x, y: x if (is_anagram(x, y)) else ""), b)
 
 
 print(is_anagram_ext("asd", "dsa", "sda"), is_anagram_ext("asd", "sda"), is_anagram_ext("asd", "sda", "dsa."))
