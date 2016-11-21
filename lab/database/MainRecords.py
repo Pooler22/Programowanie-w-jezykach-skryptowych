802ba099e6a9d998720ce1cb1e8891213002285f
pooler22 <rudypawel12@gmail.com>
2016-11-21 18:56:10 +0100

    starting prepare  exercise 6b

--- lab/database/MainRecords.py
+++ lab/database/MainRecords.py
@@ -4,11 +4,15 @@ def load_from_url(url, file_name):
     return urllib.request.urlopen(url + file_name).read().decode('utf-8').split('\n')
 
 path = "http://mmajchr.kis.p.lodz.pl/pwjs/"
-
 files = ("imiona.txt","nazwiska.txt")
 
-for file in files:
-    data = load_from_url(path, file)
-    for i in sorted(data):
-        print(i)
-    print(data.__len__())
+def exercise6a():
+    for file in files:
+        data = load_from_url(path, file)
+        for i in sorted(data):
+            print(i)
+        print(data.__len__())
+
+def exercise6b():
+    names = load_from_url(path, files[0])
+    surnames = load_from_url(path, files[1])
